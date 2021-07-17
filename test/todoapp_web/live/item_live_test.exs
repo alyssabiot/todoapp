5e0549d65a13422db2dde401db1a608b0e264562
Alyssa Biot <alyssa.biot@gmail.com>
2021-07-17 18:51:02 +0200

    remove unused show code

--- test/todoapp_web/live/item_live_test.exs
+++ test/todoapp_web/live/item_live_test.exs
@@ -6,7 +6,11 @@ defmodule TodoappWeb.ItemLiveTest do
   alias Todoapp.Todos
 
   @create_attrs %{description: "some description", is_done: true, priority: "some priority"}
-  @update_attrs %{description: "some updated description", is_done: false, priority: "some updated priority"}
+  @update_attrs %{
+    description: "some updated description",
+    is_done: false,
+    priority: "some updated priority"
+  }
   @invalid_attrs %{description: nil, is_done: nil, priority: nil}
 
   defp fixture(:item) do
@@ -78,39 +82,6 @@ defmodule TodoappWeb.ItemLiveTest do
 
       assert index_live |> element("#item-#{item.id} a", "Delete") |> render_click()
       refute has_element?(index_live, "#item-#{item.id}")
-    end
-  end
-
-  describe "Show" do
-    setup [:create_item]
-
-    test "displays item", %{conn: conn, item: item} do
-      {:ok, _show_live, html} = live(conn, Routes.item_show_path(conn, :show, item))
-
-      assert html =~ "Show Item"
-      assert html =~ item.description
-    end
-
-    test "updates item within modal", %{conn: conn, item: item} do
-      {:ok, show_live, _html} = live(conn, Routes.item_show_path(conn, :show, item))
-
-      assert show_live |> element("a", "Edit") |> render_click() =~
-               "Edit Item"
-
-      assert_patch(show_live, Routes.item_show_path(conn, :edit, item))
-
-      assert show_live
-             |> form("#item-form", item: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        show_live
-        |> form("#item-form", item: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.item_show_path(conn, :show, item))
-
-      assert html =~ "Item updated successfully"
-      assert html =~ "some updated description"
     end
   end
 end
